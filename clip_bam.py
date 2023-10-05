499db38b95ff7189cbc8daa55bb89dbb5d31dba9
maojanlin <mlin77@jhu.edu>
2023-10-05 15:44:44 -0400

    temp version of the clip_bam.py

--- clip_bam.py
+++ clip_bam.py
@@ -20,16 +20,25 @@ def get_reverse_complement(input_seq):
 
 
 def cigar_read_len(cigar_string):
+    bg_SH = 0
+    ed_SH = 0
     read_len = 0
     current_len = 0
+    bg_flag = True
     for ele in cigar_string:
         if ele.isdigit():
             current_len = int(ele) + current_len*10
         else:
             if ele == 'M' or ele == "I":
                 read_len += current_len
+            elif ele == 'S' or ele == "H":
+                if bg_flag:
+                    bg_SH = current_len
+                else:
+                    ed_SH = current_len
             current_len = 0
-    return read_len
+            bg_flag = False
+    return bg_SH, read_len, ed_SH
 
 
 def clip_fasta(f_bam, list_gene_position, list_gene_name, ref_name, \
@@ -64,7 +73,6 @@ def clip_fasta(f_bam, list_gene_position, list_gene_name, ref_name, \
     dict_gene_region = get_gene_region(list_gene_name, list_gene_position)
     J_region = dict_gene_region["J"]
     D_region = dict_gene_region["D"]
-    print(D_region)
     
     for segment in f_bam.fetch(ref_name, J_region[0], J_region[1]):
         seq_name  = segment.query_name
@@ -75,19 +83,19 @@ def clip_fasta(f_bam, list_gene_position, list_gene_name, ref_name, \
         cigar_tuples = segment.cigartuples
         cigar_string = segment.cigarstring
         cigar_states = segment.get_cigar_stats()
-        flag_forward = not segment.is_reverse
+        flag_forward = not segment.is_reverse # forward: True, reverse: False
         
-        # list_alignment is like the applying
+        # list information of the supplementary alignments
         list_alignment = segment.get_tag("SA").split(";")[:-1]
         list_alignment = [ele.split(',') for ele in list_alignment]
         list_alignment = sorted(list_alignment, key=lambda x:int(x[1]))
-        #print(seq_name, start_pos, flag_forward, sum(cigar_states[0][:2]))
-        #for SA_info in list_alignment:
-        #    print("\t", ",".join(SA_info))
-        flag_strand_contradict = False
+        # Check if the read is in the fasta file
         if dict_target_read.get(seq_name) == None:
             print("Warning!", "'"+seq_name+"'", "not exist in fasta file.")
             continue
+        
+        """
+        flag_strand_contradict = False
         if flag_forward:
             target_seq = dict_target_read[seq_name]
             for SA_info in list_alignment:
@@ -105,8 +113,54 @@ def clip_fasta(f_bam, list_gene_position, list_gene_name, ref_name, \
             print(seq_name, start_pos, flag_forward, sum(cigar_states[0][:2]))
             for SA_info in list_alignment:
                 print("=====", SA_info)
-            continue
+            continue"""
         
+        
+        seq_forward = dict_target_read[seq_name]
+        seq_reverse = get_reverse_complement(seq_forward)
+        flag_constant = False # check if there is constant gene recombination
+        for SA_info in list_alignment:
+            if SA_info[0] == ref_name and int(SA_info[1]) < start_pos: # it is a constant gene recombination
+                flag_constant = True
+                bg_SH, read_len, ed_SH = cigar_read_len(SA_info[3])
+                
+                fo.write(">" + seq_name + '/0\n')
+                if SA_info[2] == "+": # forward
+                    fo.write(seq_forward[bg_SH:bg_SH + read_len -300] + '\n')
+                else:
+                    fo.write(seq_reverse[bg_SH:bg_SH + read_len -300] + '\n')
+        target_seq = seq_forward if flag_forward else seq_reverse
+        bg_SH, read_len, ed_SH = cigar_read_len(cigar_string) # cutting from J sequence
+        """
+        if ed_SH != 0: # there are following sequence
+            fo.write(">" + seq_name + '/1\n')
+            fo.write(target_seq[:-(ed_SH+60)] + '\n')
+            if ed_SH > 300:
+                fo.write(">" + seq_name + '/2\n')
+                fo.write(target_seq[-(ed_SH-300):] + '\n')
+        else:
+            fo.write(">" + seq_name + '/1\n')
+            fo.write(target_seq + '\n')
+
+        """
+        if flag_constant: # if constant gene recombination, clip the beginning of the J gene sequence
+            const_clip = 60
+        else:
+            const_clip = 0
+            if bg_SH > 300:
+                fo.write(">" + seq_name + '/0\n')
+                fo.write(target_seq[:bg_SH] + '\n')
+        if ed_SH != 0: # there are following sequence
+            fo.write(">" + seq_name + '/1\n')
+            fo.write(target_seq[bg_SH+const_clip:-(ed_SH+60)] + '\n')
+            if ed_SH > 300:
+                fo.write(">" + seq_name + '/2\n')
+                fo.write(target_seq[-(ed_SH-300):] + '\n')
+        else:
+            fo.write(">" + seq_name + '/1\n')
+            fo.write(target_seq[bg_SH+const_clip:] + '\n')
+
+        """
         clip_dist = 0
         if list_alignment[0][0] == ref_name and int(list_alignment[0][1]) < start_pos:
             # Constant gene recombination here
@@ -127,6 +181,7 @@ def clip_fasta(f_bam, list_gene_position, list_gene_name, ref_name, \
         if cigar_read_len(list_alignment[-1][3]) > clip_dist:
             fo.write(">" + seq_name + '/2\n')
             fo.write(target_seq[-(cigar_read_len(list_alignment[-1][3])-clip_dist):] + '\n')
+        """
 
     fo.close()
 
